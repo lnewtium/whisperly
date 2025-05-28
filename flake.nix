f92cedefab102aafa3161d89d54333e40d4cca59
lnewtium <lnewtium@outlook.com>
2025-05-28 21:11:27 +0200

    build: split client & server

--- flake.nix
+++ flake.nix
@@ -5,24 +5,28 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    pkgsMusl = import pkgs.path {
-      inherit system;
-      crossSystem = {
-        config = "x86_64-unknown-linux-musl";
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      pkgsMusl = import pkgs.path {
+        inherit system;
+        crossSystem = {
+          config = "x86_64-unknown-linux-musl";
+        };
+      };
+    in
+    {
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = [
+          pkgs.gcc13
+          pkgs.ninja
+          pkgs.cmake
+          pkgs.vcpkg
+        ];
+        CMAKE_GENERATOR = "Ninja";
+        CMAKE_TOOLCHAIN_FILE = "${pkgs.vcpkg.outPath}/share/vcpkg/scripts/buildsystems/vcpkg.cmake";
       };
     };
-  in {
-    devShell.${system} = pkgs.mkShell {
-      buildInputs = [
-	pkgsMusl.gcc13
-	pkgs.ninja
-	pkgs.cmake
-	pkgs.vcpkg
-      ];
-      VCPKG_ROOT = "${pkgs.vcpkg.outPath}/share/vcpkg";
-    };
-  };
 }
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
-int main() {
+int main()
+{
+
   return 0;
 }
